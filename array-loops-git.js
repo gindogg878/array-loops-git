220a0d87a9aa180461be2ecdcc0e6e62946d28fa
Gino Torres Jr <gindogg878@icloud.com>
2023-03-07 16:26:07 -0600

    Fizz buzz answered

--- array-loops-git.js
+++ array-loops-git.js
@@ -9,15 +9,27 @@
 ////////////////////////////////
 // Get Even
 
-for (let i = 0; i <= 200; i++) {
-  if (i % 2 === 0) {
-    console.log(i);
-  }
-}
+// for (let i = 0; i <= 200; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
 ////////////////////////////////
 
 ////////////////////////////////
 // Fizz Buzz
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 5 === 0 && i % 3 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else {
+    console.log(i);
+  }
+}
 ////////////////////////////////
 
 ////////////////////////////////
