c373a9e3d6a3d27bfe8d894822cc607cd4c31a61
Gino Torres Jr <gindogg878@icloud.com>
2023-03-07 16:47:14 -0600

    wild life answered

--- array-loops-git.js
+++ array-loops-git.js
@@ -19,21 +19,42 @@
 ////////////////////////////////
 // Fizz Buzz
 
-for (let i = 1; i <= 100; i++) {
-  if (i % 5 === 0 && i % 3 === 0) {
-    console.log("FizzBuzz");
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
-  } else if (i % 3 === 0) {
-    console.log("Fizz");
-  } else {
-    console.log(i);
-  }
-}
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 5 === 0 && i % 3 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else {
+//     console.log(i);
+//   }
+// }
 ////////////////////////////////
 
 ////////////////////////////////
 // Wild Wild Life
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+const sharky = ["Sharky", "shark", 20, "Left Coast"];
+const plantee = ["Plantee", "plant", 5000, "Mordor"];
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
+
+const newArr = function (arr, newItem) {
+  arr.pop();
+  arr.push(newItem);
+};
+plantee[2] = 5001;
+console.log(plantee);
+
+newArr(wolfy, "Gotham City");
+console.log(wolfy);
+
+dart.push("Hawkins");
+console.log(dart);
+wolfy.splice(0, 1, "Gameboy");
+console.log(wolfy);
+
 ////////////////////////////////
 
 ////////////////////////////////
