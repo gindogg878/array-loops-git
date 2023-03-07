fb7096b85753343e92fb83d1ffb54f41e24fbe2f
Gino Torres Jr <gindogg878@icloud.com>
2023-03-07 17:01:20 -0600

    ninja turtles answered

--- array-loops-git.js
+++ array-loops-git.js
@@ -34,31 +34,35 @@
 
 ////////////////////////////////
 // Wild Wild Life
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
-const sharky = ["Sharky", "shark", 20, "Left Coast"];
-const plantee = ["Plantee", "plant", 5000, "Mordor"];
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
-const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+// const sharky = ["Sharky", "shark", 20, "Left Coast"];
+// const plantee = ["Plantee", "plant", 5000, "Mordor"];
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+// const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
 
-const newArr = function (arr, newItem) {
-  arr.pop();
-  arr.push(newItem);
-};
-plantee[2] = 5001;
-console.log(plantee);
+// const newArr = function (arr, newItem) {
+//   arr.pop();
+//   arr.push(newItem);
+// };
+// plantee[2] = 5001;
+// console.log(plantee);
 
-newArr(wolfy, "Gotham City");
-console.log(wolfy);
+// newArr(wolfy, "Gotham City");
+// console.log(wolfy);
 
-dart.push("Hawkins");
-console.log(dart);
-wolfy.splice(0, 1, "Gameboy");
-console.log(wolfy);
+// dart.push("Hawkins");
+// console.log(dart);
+// wolfy.splice(0, 1, "Gameboy");
+// console.log(wolfy);
 
 ////////////////////////////////
 
 ////////////////////////////////
 // Yell at the Ninja Turtles
+const ninjaTurtles = ["Leonardo", "Donatello", "Michaelangelo", "Raphael"];
+for (const value of ninjaTurtles) {
+  console.log(value.toUpperCase(value));
+}
 ////////////////////////////////
 
 ////////////////////////////////
